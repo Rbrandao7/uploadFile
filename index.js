4ccfa3fe1bb8e64d763c46fe336c4169a5e13c91
rodrigo <rodrigo2.brandao@gmail.com>
2020-07-16 12:46:04 -0400

    deploy na umbler

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000;
+const multiparty = require('multiparty')
+
+
+app.post('/api/uploadfile',(req,res)=>{
+    const form = new multiparty.Form()
+    form.parse(req,(err,fields,files)=>{
+        if(err) return res.status(500).send({error:'erro ao subir arquivo'})
+        console.log(files)
+    })
+})
 
 
 app.listen(port,(e)=>{

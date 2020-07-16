1776d53cfedd50be39c9df44aade538657b1eade
rodrigo <rodrigo2.brandao@gmail.com>
2020-07-16 13:30:10 -0400

    arquivo estatico

--- index.js
+++ index.js
@@ -2,13 +2,16 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000;
 const multiparty = require('multiparty')
+var serveStatic = require('serve-static')
 
+app.use(serveStatic('public/ftp', { 'index': ['default.html', 'default.htm'] }))
 
 app.post('/api/uploadfile',(req,res)=>{
     const form = new multiparty.Form()
     form.parse(req,(err,fields,files)=>{
         if(err) return res.status(500).send({error:'erro ao subir arquivo'})
         console.log(files)
+        res.status(210)
     })
 })
 

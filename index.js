e3f045cd369d6e4e0c130783e23e4c0aa4a7460f
rodrigo <rodrigo2.brandao@gmail.com>
2020-07-16 17:08:27 -0400

    mudanca nome arquivo upload

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const port = process.env.PORT || 3000;
 const multiparty = require('multiparty')
 const pathUtils = require('path');
 const fs = require('fs');
+var crypto = require('crypto');
+crypto.randomBytes(10, function (erro, resultado /*buffer*/) {
+  if (erro) {throw erro;}
+  console.log('Uma string aleatória de %d bytes: %s', resultado .length, resultado);
+});
 
 const imgDir = pathUtils.resolve(__dirname,'img-upload');
 if(!fs.existsSync(imgDir)) fs.mkdirSync(imgDir)
@@ -16,15 +21,15 @@ app.post('/api/uploadfile',(req,res)=>{
         if(err) return res.status(500).send({error:'erro ao subir arquivo'})
         const image = files['files[]'][0];
         const timeStamp = Date.now();
-        const dir = imgDir + '/' + timeStamp ;
-        const path = dir + '/' + image.originalFilename
+        const dir = imgDir 
+        const path = dir + '/'+timeStamp+'-'+image.originalFilename
         fs.mkdirSync(dir)
         console.log(image.path)
         console.log(path)
         fs.copyFileSync(image.path,path,(e)=>{console.log('arquivo salvo')})
 
         console.log()
-        res.status(210).send({sucess:'upload complete',urlImage:'http://ufile-com.umbler.net/img-upload/'+timeStamp+'/'+image.originalFilename})
+        res.status(210).send({sucess:'upload complete',urlImage:'http://ufile-com.umbler.net/img-upload/'+timeStamp+'-'+image.originalFilename})
     })
 })
 
